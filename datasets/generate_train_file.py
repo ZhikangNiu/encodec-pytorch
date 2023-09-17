966c676ac9cc085e2d35c2c825d8cab9515a4b42
NoFish-528 <nzk020109@163.com>
2023-09-17 00:10:51 +0000

    fix: check output parent path exists when generate train file csv

--- datasets/generate_train_file.py
+++ datasets/generate_train_file.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from pathlib import Path
 
 def generate_csv(file_dir, csv_path,mode='train'):
     # 生成file_dir下所有文件的路径
@@ -9,15 +10,35 @@ def generate_csv(file_dir, csv_path,mode='train'):
             if file.endswith('.flac') or file.endswith('.wav') and mode in root:
                 file_list.append(os.path.join(root, file))
     # 生成csv文件
+    csv_path = Path(csv_path)
+    if not csv_path.parent.exists():
+        csv_path.parent.mkdir(parents=True)
+        
     with open(csv_path, 'w') as f:
         for file in file_list:
             f.write(file + '\n')
 
+def split_train_test_csv(csv_path, threshold=0.8):
+    try:
+        import pandas as pd  
+        from sklearn.model_selection import train_test_split  
+    except ImportError as E:
+        print("please pip install pandas slearn")
+        
+    data = pd.read_csv(csv_path)  
+    train_data, test_data = train_test_split(data, train_size=threshold, random_state=42)  
+   
+    train_data.to_csv(f'{Path(csv_path).stem}_train.csv', index=False)  
+    test_data.to_csv(f'{Path(csv_path).stem}_test.csv', index=False) 
 
 if __name__ == '__main__':
     arg = argparse.ArgumentParser()
     arg.add_argument('-i','--input_file_dir', type=str, default='./LibriSpeech/train-clean-100')
     arg.add_argument('-o','--output_path', type=str, default='./librispeech_train100h.csv')
     arg.add_argument('-m','--mode', type=str, default='train',help='train,test-clean/other or dev-clean/other')
+    arg.add_argument('-s','--split', action='store_true', default=False,help='slpit dataset')
+    arg.add_argument('-t','--threshold',type=float,default=0.8)
     args = arg.parse_args()
     generate_csv(args.input_file_dir, args.output_path,args.mode)
+    if args.split:
+        split_train_test_csv(args.output_path,threshold=args.threshold)
